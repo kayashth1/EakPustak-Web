00525dab44d12ac5c67ad7d8fdbfafee73a83c21
kayashth1 <yashprof2004@gmail.com>
2025-12-09 23:35:53 +0530

    Testimonial Added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,6 @@
 import logo from './Logo.png'
 import Placeholder_g from './Placeholder_girl.jpg'
+import Placeholder_b from './placeholder_boy.jpg'
 import light_logo from './light_logo_2.png'
 import logo_dark from './logo_dark.svg'
 import cross_icon from './cross_icon.svg'
@@ -30,12 +31,12 @@ export const assets = {
 
   export const testimonialsData = [
     {
-        name: "Donald Jackman",
-        title: "Marketing Manager",
-        image: profile_img_1,
+        name: "Ritwik Mondal",
+        title: "Professor, IIT (ISM) Dhanbad",
+        image: Placeholder_b,
         alt: "Portrait of Donald Jackman",
         rating: 5,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+        text: "This student tablet is a thoughtful and practical solution to reduce academic burden. Its structured, distraction-free learning environment can genuinely help students stay organized and improve study efficiency."
     },
     {
         name: "Sanskriti",
@@ -43,14 +44,14 @@ export const assets = {
         image: Placeholder_g,
         alt: "Portrait of Richard Nelson",
         rating: 4,
-        text: "I am Sanskriti, and from the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is truly unmatched."
+        text: "The student tablet is incredibly practical for modern classrooms. My students stay more organized, waste less time managing books, and engage better with lessons. It’s a simple, effective upgrade to traditional learning."
     },
     {
-        name: "James Washington",
-        title: "Co-Founder",
-        image: profile_img_3,
+        name: "Prem Mehtre",
+        title: "Senior Advisor at NGO",
+        image: Placeholder_b,
         alt: "Portrait of James Washington",
         rating: 5,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
+        text: "This tablet can meaningfully support underprivileged learners by making quality study material accessible and affordable. It’s a well-designed, student-friendly tool with real potential to improve learning outcomes"
     }
 ];
